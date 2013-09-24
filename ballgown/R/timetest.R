b5c758e3d5e67859ddb683ab4b7a7d38f15cb30a
Alyssa Frazee <acfrazee@gmail.com>
2013-09-24 17:50:22 -0400

    implements intial version of timecourse stat test (UNTESTED FUNCTION)

---
+++ ballgown/R/timetest.R
@@ -0,0 +1,45 @@
+# statistical test for timecourse data in ballgown
+
+timetest = function(gown, mod = NULL, mod0 = NULL, df = 4, 
+	feature = c("gene", "exon", "intron", "transcript"),
+	timevar){
+
+	require(matrixStats)
+	require(splines)
+
+	feature = match.arg(feature)
+
+	if(feature == "gene"){
+        gnames = indexes(gown)$t2g$g_id
+        inds_by_gene = split(seq(along=gnames), gnames)
+        expr = t(sapply(inds_by_gene, function(i){
+        	colSums(texpr(gown)[i,-c(1:10),drop=FALSE])}
+        ))
+	}
+	if(feature == "exon") expr = eexpr(gown)[,-c(1:5)]
+	if(feature == "intron") expr = iexpr(gown)[,-c(1:5)]
+	if(feature == "transcript") expr = texpr(gown)[,-c(1:10)]
+
+	n = ncol(expr)
+
+    ## library size adjustment
+	med_expr = colMedians(expr)
+
+    if(is.null(mod) & is.null(mod0)) {
+    	## just run simple test: is time important or not?
+        time_col_ind = which(names(pData(gown)) == timevar)
+        if(length(time_col_ind) == 0) stop("invalid time variable name")
+
+        ## create time covariate
+        tme = pData(gown)[,time_col_ind]
+        
+        ## create model matrices
+        mod0 = model.matrix(~ med_expr)
+        mod = model.matrix(~ ns(tme, df = df) + med_expr)
+    }
+
+    results = f.pvalue(log2(expr + 1), mod, mod0)
+
+    return(data.frame(id=rownames(expr), pval = results))
+
+}
