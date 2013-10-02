5d32defad8e7091c553aee12f52140f833cceb5a
Alyssa Frazee <acfrazee@gmail.com>
2013-10-02 14:12:56 -0400

    fixes potential bug in texpr method

--- ballgown/R/ballgown-class.R
+++ ballgown/R/ballgown-class.R
@@ -202,7 +202,7 @@ setMethod("texpr", "ballgown", function(x, meas="all"){
   meas = match.arg(meas, c("cov","FPKM","all"))
   if(meas!="all"){
     expr = data(x)$trans[,-c(1:10)]
-    expr = expr[,grepl(meas, colnames(expr))]
+    expr = expr[,sapply(colnames(expr), function(x) strsplit(x,split="\\.")[[1]][1]==meas)]
     rownames(expr) = data(x)$trans$t_id
   }else{
     expr = data(x)$trans
