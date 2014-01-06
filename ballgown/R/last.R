41937fd9c6a31311f2458c7db303d7e4196ff730
Alyssa Frazee <acfrazee@gmail.com>
2014-01-06 13:51:45 -0500

    documents last

--- ballgown/R/last.R
+++ ballgown/R/last.R
@@ -1,1 +1,10 @@
-last = function(x) return(tail(x,n=1))
+#' get the last element
+#'
+#' @param x anything you can call \code{tail} on (vector, data frame, etc.)
+#' @return the last element of \code{x}
+#' @details this function is made of several thousand lines of complex code, so be sure to read it carefully.
+#' @author Alyssa Frazee
+#' @export
+
+
+last = function(x) return(tail(x, n=1))
