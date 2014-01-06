b2841898a51752af8758a2034b5804ec105edfc3
Alyssa Frazee <acfrazee@gmail.com>
2014-01-06 11:37:23 -0500

    fixes whitespace

--- ballgown/R/clusterTranscripts.R
+++ ballgown/R/clusterTranscripts.R
@@ -1,45 +1,54 @@
+#' group a gene's assembled transcripts into clusters
+#'
+#' @param gene name of gene whose transcripts will be clustered.  When using Cufflinks output, usually of the form \code{"XLOC_######"}
+#' @param gown ballgown object containing experimental data
+#' @k number of clusters to use
+#' @return vector of nucleotide positions included in the transcript
+#' @seealso \code{\link{transcriptOverlaps}} which is this function's wrapper
+#' @export
 clusterTranscripts = function(gene, gown, k=NULL, method="hclust"){
-  txnames = indexes(gown)$t2g$t_id[indexes(gown)$t2g$g_id == gene]
-  strucnames = as.numeric(substr(names(structure(gown)$trans),3,nchar(names(structure(gown)$trans))))
-  inds = which(strucnames %in% txnames)
-  tx = structure(gown)$trans[inds]
-  chr = data(gown)$trans$chr[inds[1]] #add error check later, in case the tx's are on different chromosomes.  ugh.
+    txnames = indexes(gown)$t2g$t_id[indexes(gown)$t2g$g_id == gene]
+    strucnames = as.numeric(substr(names(structure(gown)$trans),3,nchar(names(structure(gown)$trans))))
+    inds = which(strucnames %in% txnames)
+    tx = structure(gown)$trans[inds]
+    chr = data(gown)$trans$chr[inds[1]] #add error check later, in case the tx's are on different chromosomes.  ugh.
+
+    covind = unique(as.numeric(lapply(tx, function(x) runValue(seqnames(x)))))
+    covs = lapply(tx, function(x) coverage(x)[[covind]])
   
-  covind = unique(as.numeric(lapply(tx, function(x) runValue(seqnames(x)))))
-  covs = lapply(tx, function(x) coverage(x)[[covind]])
-  
-  startpos = unlist(lapply(covs, function(x) runLength(x)[1]+1))
-  endpos = unlist(lapply(covs, function(x) sum(runLength(x))))
-  minbp = min(startpos)
-  maxbp = max(endpos)
-  
-  compactrles = lapply(covs, function(x) Rle(values = runValue(x)[-1], lengths = runLength(x)[-1]))
-  expanded = lapply(compactrles, IRanges::as.vector)
-  tnames = names(expanded)
-  expanded = lapply(1:length(expanded), function(i){
-    out = expanded[[i]]
-    if(startpos[i] > minbp){
-      out = c(rep(0, startpos[i]-minbp), out)
+    startpos = unlist(lapply(covs, function(x) runLength(x)[1]+1))
+    endpos = unlist(lapply(covs, function(x) sum(runLength(x))))
+    minbp = min(startpos)
+    maxbp = max(endpos)
+
+    compactrles = lapply(covs, function(x) Rle(values = runValue(x)[-1], lengths = runLength(x)[-1]))
+    expanded = lapply(compactrles, IRanges::as.vector)
+    tnames = names(expanded)
+    expanded = lapply(1:length(expanded), function(i){
+        out = expanded[[i]]
+        if(startpos[i] >= minbp){
+            out = c(rep(0, startpos[i]-minbp), out)
+        }
+        if(endpos[i] < maxbp){
+            out = c(out, rep(0, maxbp-endpos[i]))
+        }
+        return(out)
+    })
+
+    d = dist(matrix(unlist(expanded), nrow=length(expanded), byrow=TRUE))
+    if(is.null(k)) k = ceiling(sqrt(length(tnames)/2))
+
+    if(method=="hclust"){
+        h = hclust(d)
+        groups = cutree(h, k=k)
+        pctvar = NULL
+    } 
+    
+    if(method=="kmeans"){
+        km = kmeans(as.matrix(d), centers=k)
+        groups = as.numeric(km$cluster)
+        pctvar = km$betweenss/km$totss
     }
-    if(endpos[i] < maxbp){
-      out = c(out, rep(0, maxbp-endpos[i]))
-    }
-    return(out)
-  })
-  d = dist(matrix(unlist(expanded), nrow=length(expanded), byrow=TRUE))
-  if(is.null(k)) k = ceiling(sqrt(length(tnames)/2))
-  
-  if(method=="hclust"){
-    h = hclust(d)
-    groups = cutree(h, k=k)
-    pctvar = NULL
-  }
-  
-  if(method=="kmeans"){
-    km = kmeans(as.matrix(d), centers=k)
-    groups = as.numeric(km$cluster)
-    pctvar = km$betweenss/km$totss
-  }
-  
-  return(list(clusters = data.frame(cluster=groups, tname=tnames, tid=txnames), pctvar = pctvar))
+
+    return(list(clusters = data.frame(cluster=groups, tname=tnames, tid=txnames), pctvar = pctvar))
 }
