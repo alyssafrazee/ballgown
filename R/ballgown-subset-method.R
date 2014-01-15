592b2d5088a77bcd5a5ef3db416d2663833a3bdd
Alyssa Frazee <acfrazee@gmail.com>
2014-01-15 13:54:01 -0500

    fixes subset method so it (a) works and (b) also handles phenotype subsets

--- R/ballgown-subset-method.R
+++ R/ballgown-subset-method.R
@@ -1,27 +1,67 @@
 # subset method for ballgown
-setMethod("subset", "ballgown", function(x, cond, global=TRUE){
+setMethod("subset", "ballgown", function(x, cond, genomesubset=TRUE){
+    stopifnot(class(cond) == 'character')
 
-    ctext = ifelse(global, deparse(substitute(cond)), cond) # this means that inside another function, you can make a string argument to give to subset.
-    trans = subset(data(x)$trans, eval(parse(text=ctext)))  
+    # if you are subsetting by something in the genome (say, a chromosome):
+    if(genomesubset){
+        trans = subset(data(x)$trans, eval(parse(text=cond)))  
+        thetx = trans$t_id
     
-    thetx = trans$t_id
+        inttmp = split(indexes(x)$i2t$i_id, indexes(x)$i2t$t_id)
+        theint = as.numeric(unique(unlist(inttmp[names(inttmp) %in% thetx])))
+        intron = subset(data(x)$intron, i_id %in% theint)
     
-    inttmp = split(indexes(x)$i2t$i_id, indexes(x)$i2t$t_id)
-    theint = as.numeric(unique(unlist(inttmp[names(inttmp) %in% thetx])))
-    intron = subset(data(x)$intron, i_id %in% theint)
+        extmp = split(indexes(x)$e2t$e_id, indexes(x)$e2t$t_id)
+        theex = as.numeric(unique(unlist(extmp[names(extmp) %in% thetx])))
+        exon = subset(data(x)$exon, e_id %in% theex)
     
-    extmp = split(indexes(x)$e2t$e_id, indexes(x)$e2t$t_id)
-    theex = as.numeric(unique(unlist(extmp[names(extmp) %in% thetx])))
-    exon = subset(data(x)$exon, e_id %in% theex)
+        e2t = subset(indexes(x)$e2t, t_id %in% thetx)
+        i2t = subset(indexes(x)$i2t, t_id %in% thetx)
+        t2g = subset(indexes(x)$t2g, t_id %in% thetx)
     
-    e2t = subset(indexes(x)$e2t, t_id %in% thetx)
-    i2t = subset(indexes(x)$i2t, t_id %in% thetx)
-    t2g = subset(indexes(x)$t2g, t_id %in% thetx)
+        introngr = structure(x)$intron[elementMetadata(structure(x)$intron)$id %in% theint]
+        exongr = structure(x)$exon[elementMetadata(structure(x)$exon)$id %in% theex]
+        grltxids = sapply(names(structure(x)$trans), function(a) as.numeric(substr(a, 3, nchar(a))))
+        transgrl = structure(x)$trans[grltxids %in% thetx]
     
-    introngr = structure(x)$intron[elementMetadata(structure(x)$intron)$id %in% theint]
-    exongr = structure(x)$exon[elementMetadata(structure(x)$exon)$id %in% theex]
-    grltxids = sapply(names(structure(x)$trans), function(a) as.numeric(substr(a, 3, nchar(a))))
-    transgrl = structure(x)$trans[grltxids %in% thetx]
-    
-    return(new("ballgown", data = list(intron=intron, exon=exon, trans=trans), indexes = list(e2t=e2t, i2t=i2t, t2g=t2g, bamfiles = indexes(x)$bamfiles, pData = indexes(x)$pData), structure=list(intron = introngr, exon=exongr, trans=transgrl), dirs = dirs(x), mergedDate=mergedDate(x)))
+        return(new("ballgown", data=list(intron=intron, exon=exon, trans=trans), 
+            indexes=list(e2t=e2t, i2t=i2t, t2g=t2g, bamfiles=indexes(x)$bamfiles, pData=indexes(x)$pData), 
+            structure=list(intron=introngr, exon=exongr, trans=transgrl), dirs=dirs(x), mergedDate=mergedDate(x)))
+    }else{
+        # you're doing a phenotype subset
+        # structure, some indexes, dirs, and mergedDate stay the same
+        # change: data, indexes(pData), and indexes(bamfiles)
+        
+        ## pData
+        newpd = subset(pData(x), eval(parse(text=cond)))
+        
+        ## bamfiles
+        newsampnames = newpd[,1]
+        rowIndsToKeep = which(pData(x)[,1] %in% newsampnames)
+        if(!is.null(indexes(x)$bamfiles)){
+            newbamfiles = indexes(x)$bamfiles[rowIndsToKeep]
+        }else{
+            newbamfiles = NULL
+        }
+
+        ## transcript data
+        txcolsamples = sapply(names(texpr(x, 'all')), getsamp, USE.NAMES=FALSE)
+        txKeepCols = c(1:10, which(txcolsamples %in% newsampnames))
+        newtdat = texpr(x, 'all')[,txKeepCols]
+
+        ## exon data
+        excolsamples = sapply(names(eexpr(x, 'all')), getsamp, USE.NAMES=FALSE)
+        exKeepCols = c(1:5, which(excolsamples %in% newsampnames))
+        newedat = eexpr(x, 'all')[,exKeepCols]
+
+        ## intron data
+        icolsamples = sapply(names(iexpr(x, 'all')), getsamp, USE.NAMES=FALSE)
+        iKeepCols = c(1:5, which(icolsamples %in% newsampnames))
+        newidat = iexpr(x, 'all')[,iKeepCols]
+
+        return(new("ballgown", data=list(intron=newidat, exon=newedat, trans=newtdat), 
+            indexes=list(e2t=indexes(x)$e2t, i2t=indexes(x)$i2t, t2g=indexes(x)$t2g, bamfiles=newbamfiles, pData=newpd),
+            structure=list(intron=structure(x)$intron, exon=structure(x)$exon, trans=structure(x)$trans),
+            dirs=dirs(x)[rowIndsToKeep], mergedDate=mergedDate(x)))
+    }
 } )
