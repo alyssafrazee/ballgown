3f0aba19ae291af0364abab65875d421ac77feca
Alyssa Frazee <acfrazee@gmail.com>
2014-02-12 16:29:55 -0500

    fixes and documents gffReadGR

--- R/gffReadGR.R
+++ R/gffReadGR.R
@@ -3,14 +3,22 @@
 #' (very) light wrapper for rtracklayer::import
 #'  
 #' @param gtf name of GTF/GFF file on disk
-#' @return object of class \code{GRanges} representing the genomic features in \code{gtf}
-#' @seealso \code{\link{gffRead}} for reading in a GTF file as a data frame rather than GRanges.
+#' @param splitByTranscript if \code{TRUE}, return a \code{GRangesList} of transcripts; otherwise return a \code{GRanges} object containing all genomic features in \code{gtf}. Default \code{FALSE}.
+#' @param identifier name of transcript identifier column of \code{attributes} field in \code{gtf}. Default \code{"transcript_id"}. Only used if \code{splitByTranscript} is \code{TRUE}.
+#' @param sep field separator in the \code{attributes} field of \code{gtf}. Default \code{"; "} (semicolon + space). Only used if \code{splitByTranscript} is \code{TRUE}.
+#' @return if \code{splitByTranscript} is \code{FALSE}, an object of class \code{GRanges} representing the genomic features in \code{gtf}. If \code{splitByTranscript} is TRUE, an object of class \code{GRangesList}, where each element is a \code{GRanges} object corresponding to an annotated transcript (designated in \code{names}).
+#' @seealso \code{\link{gffRead}} for reading in a GTF file as a data frame rather than a \code{GRanges}/\code{GRangesList} object.
 #' @author Alyssa Frazee
 #' @export
-gffReadGR = function(gtf){
+gffReadGR = function(gtf, splitByTranscript=FALSE, identifier='transcript_id', sep='; '){
     require(rtracklayer)
     con = file(gtf)
     ret = import(con, format='GFF')
-    on.exit(close(con))
+    if(splitByTranscript){
+        ret = subset(ret, type == 'exon')
+        transcript = getAttributeField(as.character(ret$group), identifier, sep)
+        ret = split(ret, transcript)
+    }
+    on.exit(suppressWarnings(close(con)))
     return(ret)
 }
