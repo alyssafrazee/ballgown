35970c08c0f143a974da43b34cdd9d72e1e7e453
Alyssa Frazee <acfrazee@gmail.com>
2014-02-12 16:10:51 -0500

    adds new function, annotate_assembly

---
+++ R/annotate_assembly.R
@@ -0,0 +1,47 @@
+#' match assembled transcripts to annotated transcripts
+#' 
+#' @param assembled \code{GRangesList} object representing assembled transcripts
+#' @param annotated \code{GRangesList} object representing annotated transcripts
+#' @details If \code{gown} is a \code{ballgown} object, \code{assembled} can be \code{structure(gown)$trans} (or any subset). You can generate a \code{GRangesList} object containing annotated transcripts from a gtf file using the \link{\code{gffReadGR}} function and setting \code{splitByTranscripts=TRUE}.
+#' @return data frame, where each row contains \code{assembledInd} and \code{annotatedInd} (indexes of overlapping transcripts in \code{assembled} and \code{annotated}), and the percent overlap between the two transcripts.
+#' 
+#' @author Alyssa Frazee
+#' @export
+
+
+annotate_assembly = function(assembled, annotated){
+    
+    stopifnot(class(annotated) == 'GRangesList' & class(assembled) == 'GRangesList')
+
+    # get rid of metadata:
+    annotatedUL = unlist(annotated)
+    mcols(annotatedUL) = NULL
+    annotated = split(annotatedUL, names(annotatedUL))
+
+    # for assembly: get rid of metadata
+    assembledUL = unlist(assembled)
+    mcols(assembledUL) = NULL
+    assembled = split(assembledUL, names(assembledUL))
+
+    # find the overlaps:
+    ol = findOverlaps(assembled, annotated)
+
+    # get lists of corresponding assembled/annotated transcripts:
+    assembled_sort = assembled[queryHits(ol)]
+    annotated_sort = annotated[subjectHits(ol)]
+
+    # concatenate the appropriate GRanges objects (for overlapping transcripts)
+    assembled_overlapIDs = rep(1:length(ol), times=elementLengths(assembled_sort))
+    annotated_overlapIDs = rep(1:length(ol), times=elementLengths(annotated_sort))
+    all_transcripts = c(unlist(assembled_sort), unlist(annotated_sort))
+    overlapping = split(all_transcripts, c(assembled_overlapIDs, annotated_overlapIDs))
+    
+    # calculate percentage overlap:
+    coverages = coverage(ranges(overlapping))
+    runvals = runValue(coverages)
+    runlengths = runLength(coverages)
+    pct = sum(runlengths[runvals==2]) / sum(runlengths[runvals==1 | runvals==2])
+
+    return(data.frame(assembledInd=queryHits(ol), annotatedInd=subjectHits(ol), percent=pct))
+}
+
