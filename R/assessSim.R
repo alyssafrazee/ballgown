32f89daacad2c388ca2ddf2dde75427b41bee7b9
Alyssa Frazee <acfrazee@gmail.com>
2014-02-14 14:03:04 -0500

    made assessSim a lot faster using new vectorized annotate_assembly function

--- R/assessSim.R
+++ R/assessSim.R
@@ -38,83 +38,46 @@ assessSim = function(bg, bgresults, annotation, chr, trulyDEids, cuffdiffFile, q
     }
     
     degtf = subset(annotsub, tx %in% trulyDEids)
-    #stopifnot(length(unique(degtf$tx)) == length(unique(trulyDEids))) ## ensembl has some weirdness here - FIX LATER
-    nondegtf = subset(annotsub, !(tx %in% trulyDEids))
+    degr = split(GRanges(seqnames = Rle(chr), ranges = IRanges(start = degtf$start, end = degtf$end), strand = degtf$strand), degtf$tx)
 
-    degr = split(GRanges(seqnames=Rle(chr),
-        ranges=IRanges(start=degtf$start, end=degtf$end),
-        strand=degtf$strand), degtf$tx)
-    nondegr = split(GRanges(seqnames=Rle(chr),
-        ranges=IRanges(start=nondegtf$start, end=nondegtf$end),
-        strand=nondegtf$strand), nondegtf$tx)
-
-    deOverlaps = findOverlaps(assemblygr, degr)
-    nondeOverlaps = findOverlaps(assemblygr, nondegr)
-
-    depercent = function(i){
-        t1 = degr[[subjectHits(deOverlaps)[i]]]
-        t2 = assemblygr[[queryHits(deOverlaps)[i]]]
-        t1chr = as.character(runValue(seqnames(t1)))
-        t2chr = as.character(runValue(seqnames(t2)))
-        t1good = GRanges(seqnames=Rle(t1chr), ranges=ranges(t1), strand=strand(t1))
-        t2good = GRanges(seqnames=Rle(t2chr), ranges=ranges(t2), strand=strand(t2))
-        return(pctOverlap(t1good, t2good))
-    }
-
-    pctDE = sapply(1:length(deOverlaps), depercent)
-    
-    corresponding_to_de = data.frame(
-        assembled=queryHits(deOverlaps), 
-        annotated=subjectHits(deOverlaps), 
-        overlap=pctDE)
-    if(any(!(1:length(trulyDEids) %in% corresponding_to_de$annotated))){
-        message('some DE transcripts did not overlap any assembled transcripts. Indexes:')
-        print(which(!(1:length(trulyDEids) %in% corresponding_to_de$annotated)))
-    }
-
-    ol_list = split(corresponding_to_de[,c(1,3)], 
-        corresponding_to_de$annotated)
-    find_correct = function(x){
-        return(x[which(x[,2]==max(x[,2])),1])
+    deoverlaps = annotate_assembly(assemblygr, degr)
+    ol_list = split(deoverlaps[, c(1, 3)], deoverlaps[,2])
+    find_correct = function(x) {
+        return(x[which(x[, 2] == max(x[, 2])), 1])
     }
     truly_de = lapply(ol_list, find_correct)
+    
+    # make sure there's no ties for "closest assembled transcript"
     num_max = sapply(truly_de, length)
     stopifnot(all(num_max == 1))
 
-    bgtxids = texpr(bg,'all')$t_name[match(bgresults$id, texpr(bg,'all')$t_id)]
+    bgtxids = texpr(bg, "all")$t_name[match(bgresults$id, texpr(bg, "all")$t_id)]
 
     ### BALLGOWN IDS that should and should not be DE:
     de_ids = unique(as.numeric(names(assemblygr)[as.numeric(truly_de)]))
-    non_de_ids = setdiff(unique(texpr(bg,'all')$t_id), as.numeric(de_ids))
+    non_de_ids = setdiff(unique(texpr(bg, "all")$t_id), as.numeric(de_ids))
 
-    # load in cuffdiff results:
-    cuff = read.table(cuffdiffFile, sep='\t', header=TRUE)
-    cuffok = subset(cuff, status=='OK') 
-
+    # load in cuffdiff results
+    cuff = read.table(cuffdiffFile, sep = "\t", header = TRUE)
+    cuffok = subset(cuff, status == "OK")
+    
     if(length(qcut) == 1){
         cuff_decalls = as.character(cuffok$test_id[cuffok$q_value < qcut])
-        # translate cufflinks DE calls into ballgown transcript ids (numeric):
-        cuff_decalls = texpr(bg,'all')$t_id[match(cuff_decalls, texpr(bg,'all')$t_name)]
+        cuff_decalls = texpr(bg, "all")$t_id[match(cuff_decalls, texpr(bg, "all")$t_name)]
         bg_decalls = bgresults$id[which(bgresults$qval < qcut)]
         bg_decalls = as.numeric(as.character(bg_decalls))
-
-        ### sensitivity
         bgsens = sum(de_ids %in% bg_decalls)/length(de_ids)
         cuffsens = sum(de_ids %in% cuff_decalls)/length(de_ids)
-
-        ### specificity
         bgspec = sum(!(non_de_ids %in% bg_decalls))/length(non_de_ids)
-        cuffspec = sum(!(non_de_ids %in% cuff_decalls))/length(non_de_ids) 
-
-        ### false discovery rates
+        cuffspec = sum(!(non_de_ids %in% cuff_decalls))/length(non_de_ids)
         bgfdr = sum(!(bg_decalls %in% de_ids))/length(bg_decalls)
-        cufffdr = sum(!(cuff_decalls %in% de_ids))/length(cuff_decalls) 
+        cufffdr = sum(!(cuff_decalls %in% de_ids))/length(cuff_decalls)
+        return(list(ballgownsens = bgsens, cuffdiffsens = cuffsens, 
+            ballgownspec = bgspec, cuffdiffspec = cuffspec, ballgownfdr = bgfdr, 
+            cuffdifffdr = cufffdr))
+    }
 
-        return(list(ballgownsens=bgsens, cuffdiffsens=cuffsens, 
-            ballgownspec=bgspec, cuffdiffspec=cuffspec,
-            ballgownfdr=bgfdr, cuffdifffdr=cufffdr))
-        }
-    ## else, make ROC plots:
+    # else, make ROC plots
     bgsens = cuffsens = bgspec = cuffspec = NULL
     for(i in 1:length(qcut)){
         cuff_decalls = as.character(cuffok$test_id[cuffok$q_value < qcut[i]])
@@ -133,30 +96,3 @@ assessSim = function(bg, bgresults, annotation, chr, trulyDEids, cuffdiffFile, q
             ballgownspec=bgspec, cuffdiffspec=cuffspec))
     }
 }
-
-# cuffFile = "~/Desktop/simcuff.txt"
-# bgresults = stattest(simgown, feature="transcript", meas="FPKM", covariate="group")
-# annFile = "~/Google Drive/hopkins/research/_cufflinks visualization project/genes-clean-small.gtf"
-# de1 = gffRead('~/Google Drive/hopkins/research/_cufflinks visualization project/de-tx1.gtf')
-# de1 = subset(de1, feature=="exon")
-# de2 = gffRead('~/Google Drive/hopkins/research/_cufflinks visualization project/de-tx2.gtf')
-# de2 = subset(de2, feature=="exon")
-# de1$tx = getAttributeField(de1$attributes, "transcript_id")
-# de2$tx = getAttributeField(de2$attributes, "transcript_id")
-# trulyDEids = unique(c(de1$tx,de2$tx))
-
-# assessSim(bg=simgown, 
-#     bgresults=bgresults, 
-#     annotation=annFile, 
-#     chr="22", 
-#     trulyDEids=trulyDEids,
-#     cuffdiffFile=cuffFile)
-
-
-#assessSim(bg=simgown, bgresults=bgresults, annotation=annFile, 
- #    chr="22", trulyDEids=trulyDEids, cuffdiffFile=cuffFile,UCSC=TRUE)
-# assessSim(bg=simgown, bgresults=bgresults, annotation=annFile, 
-#      chr="22", trulyDEids=trulyDEids, cuffdiffFile=cuffFile, 
-#      qcut=seq(0,1,by=0.01),UCSC=FALSE)
-
-
