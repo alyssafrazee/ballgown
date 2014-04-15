d4e8ff7294c0e8270f1e300b4a44107894b403c0
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 12:03:10 -0400

    NB command now vectorized

--- polyester/R/NB.R
+++ polyester/R/NB.R
@@ -1,10 +1,20 @@
-# function to draw reads from negative binomial
-
-NB = function(inds, basemeans, dispersion_param, seed=NULL){
+#'  Negative binomial draws
+#'
+#'<full description>
+#' @param  basemeans <what param does>
+#' @param  dispersion_param <what param does>
+#' @param  seed=NULL <what param does>
+#' @export
+#' @keywords
+#' @seealso
+#' @return
+#' @alias
+#' @examples \dontrun{
+#'
+#'}
+NB = function(basemeans, dispersion_param, seed=NULL){
     if(!is.null(seed)) set.seed(seed)
-    numreads = sapply(inds, function(x){
-        rnbinom(n = 1, mu = basemeans[x], size = dispersion_param)
-    }, USE.NAMES=FALSE)
+    numreads = rnbinom(n = length(basemeans), mu = basemeans, size = dispersion_param)
     numreads[numreads == 0] = 1
     return(numreads)
 }
