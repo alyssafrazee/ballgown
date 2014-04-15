301dea723989c5140a38f43ddcf6cf98f09ecc96
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 13:55:45 -0400

    adds docs for simulate_experiment_countmat

--- polyester/R/simulate_experiment_countmat.R
+++ polyester/R/simulate_experiment_countmat.R
@@ -1,19 +1,26 @@
-#'<brief desc>
+#' Simulate RNA-seq experiment 
 #'
 #'<full description>
-#' @param fasta <what param does>
-#' @param  readmat <what param does>
-#' @param  outdir <what param does>
-#' @param  fraglen=250 <what param does>
-#' @param  fragsd=25 <what param does>
-#' @param  readlen=100 <what param does>
-#' @param  error_rate=0.005 <what param does>
-#' @param  paired=TRUE <what param does>
+#' @param fasta path to FASTA file containing transcripts from which to simulate reads 
+#' @param readmat matrix with rows representing transcripts and columns representing samples. 
+#' Entry i,j specifies how many reads to simulate from transcript i for sample j.
+#' @param outdir character, path to folder where simulated reads should be written. Should end with "/" if specified. If unspecified, reads are written to the working directory.
+#' @param fraglen Mean RNA fragment length. Sequences will be read off the end(s) of these fragments.
+#' @param fragsd Standard deviation of fragment lengths. 
+#' @param readlen Read length
+#' @param error_rate=0.005 Sequencing error rate. Must be between 0 and 1. A uniform error model is assumed. 
+#' @param paired If \code{TRUE}, paired-end reads are simulated; else single-end reads are simulated.
 #' @export
 #' @examples \dontrun{
-#'
+#' fastapath = system.file("data", "chr22.fa", package="polyester")
+#' numtx = count_transcripts(fastapath)
+#' readmat = matrix(20, ncol=10, nrow=numtx)
+#' readmat[1:30, 1:5] = 40
+#' 
+#' simulate_experiment_countmat(fastapath, readmat, outdir="./data/")
 #'}
-simulate_experiment_countmat = function(fasta, readmat, outdir, fraglen=250, fragsd=25, readlen=100, error_rate=0.005, paired=TRUE){
+simulate_experiment_countmat = function(fasta, readmat, outdir="", 
+    fraglen=250, fragsd=25, readlen=100, error_rate=0.005, paired=TRUE){
 
     transcripts = readDNAStringSet(fasta)
 
@@ -34,7 +41,7 @@ simulate_experiment_countmat = function(fasta, readmat, outdir, fraglen=250, fra
 
         #write read pairs
         write_reads(reads, readlen=readlen, 
-            fname=paste0(outdir, '/sample_', sprintf('%02d', i)), 
+            fname=paste0(outdir, 'sample_', sprintf('%02d', i)), 
             paired=paired)
     }
 }
