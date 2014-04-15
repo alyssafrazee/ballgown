2712c22388b4818ab48ab79df16d8b4710dfc586
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 16:49:55 -0400

    documents add_error

--- polyester/R/add_error.R
+++ polyester/R/add_error.R
@@ -1,26 +1,25 @@
-### function to add error to reads/fragments
-
-#'<brief desc>
+#' add sequencing error to simulated reads
 #'
-#'<full description>
-#' @param tFrags <what param does>
-#' @param  error_rate = 0.005 <what param does>
+#' simulate sequencing error by randomly changing the sequenced nucleotide on some of the reads 
+#' @param tFrags DNAStringSet representing sequencing reads
+#' @param  error_rate error probability
 #' @export
-#' @return fragments with error added
-#' @examples \dontrun{
-#'
-#'}
+#' @return DNAStringSet equivalent to \code{tFrags} but with random sequencing errors inserted
 add_error = function(tFrags, error_rate = 0.005){
-    adj_error = error_rate*4/3 # based on random read selection
+    adj_error = error_rate*4/3 
+    #^so you don't have to choose *another* nucleotide for an error: just choose *a* nucleotide.
     
     allSeq = unlist(tFrags)
     insertLocs = Rle(sample(c(TRUE,FALSE), size = length(allSeq), 
            replace=TRUE, prob = c(adj_error, 1-adj_error)))
   
-    newletters = DNAString(paste(sample(c("A", "C", "G", "T"), sum(insertLocs), replace=TRUE), collapse="") )
+    newletters = DNAString(
+        paste(sample(c("A", "C", "G", "T"), sum(insertLocs), replace=TRUE), collapse="") )
     allSeq = replaceLetterAt(allSeq, insertLocs, newletters)
     
-    eFrags = DNAStringSet(allSeq, start=c(1, cumsum(width(tFrags))[-length(tFrags)]), width=width(tFrags))
+    eFrags = DNAStringSet(allSeq, 
+        start=c(1, cumsum(width(tFrags))[-length(tFrags)]), 
+        width=width(tFrags))
     names(eFrags) = names(tFrags)
     return(eFrags)
 }
