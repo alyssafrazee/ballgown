deaaa70969499dc0e649e84fe29891796566d073
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 17:21:19 -0400

    simulate_experiment lines are all <100 char long + updated example

--- polyester/R/simulate_experiment.R
+++ polyester/R/simulate_experiment.R
@@ -1,16 +1,20 @@
 #' simulate RNA-seq experiment using negative binomial model
 #'
-#' create FASTA files containing RNA-seq reads simulated from provided transcripts, with optional differential expression between two groups
+#' create FASTA files containing RNA-seq reads simulated from provided transcripts, with optional 
+#' differential expression between two groups
 #' @param fasta path to FASTA file containing transcripts from which to simulate reads 
 #' @param  num_reps How many biological replicates should be in each group? If \code{num_reps} 
 #' is an integer, \code{num_reps} replicates will be simulated in each group. 
 #' Otherwise, \code{num_reps} can be a length-2 vector, where \code{num_reps[1]} 
 #' and \code{num_reps[2]} replicates will be simulated in each of the two groups.
-#' @param  fraglen Mean RNA fragment length. Sequences will be read off the end(s) of these fragments.
+#' @param  fraglen Mean RNA fragment length. Sequences will be read off the end(s) of these 
+#' fragments.
 #' @param  fragsd Standard deviation of fragment lengths. 
 #' @param  readlen Read length.
-#' @param  error_rate Sequencing error rate. Must be between 0 and 1. A uniform error model is assumed. 
-#' @param  paired If \code{TRUE}, paired-end reads are simulated; else single-end reads are simulated.
+#' @param  error_rate Sequencing error rate. Must be between 0 and 1. A uniform error model is 
+#' assumed. 
+#' @param  paired If \code{TRUE}, paired-end reads are simulated; else single-end reads are 
+#' simulated.
 #' @param  reads_per_transcript baseline mean number of reads to simulate 
 #' from each transcript. Can be an integer, in which case this many reads
 #' are simulated from each transcript, or an integer vector whose length
@@ -21,12 +25,14 @@
 #' samples. Fold change < 1 means transcript is overexpressed in the last
 #' \code{num_reps} (or \code{num_reps[2]}) samples. The change is in the mean
 #' number of reads generated from the transcript, between groups.
-#' @param  dispersion_param the negative binomial \code{size} distribution (see \code{\link{NegBinomial}})
+#' @param  dispersion_param the negative binomial \code{size} distribution 
+#' (see \code{\link{NegBinomial}})
 #' of the number of reads drawn per transcript. If left blank, defaults to 
 #' \code{reads_per_transcript / 3}. Negative binomial variance is mean + mean^2 / size.
-#' @param  outdir character, path to folder where simulated reads should be written. Should end with "/" if specified. If unspecified, reads are written to current working directory.
+#' @param  outdir character, path to folder where simulated reads should be written. Should end 
+#' with "/" if specified. If unspecified, reads are written to current working directory.
 #' @export
-#' @examples \dontrun{
+#' @examples
 #' ## simulate a few reads from chromosome 22
 #' 
 #' fastapath = system.file("data", "chr22.fa", package="polyester")
@@ -34,9 +40,9 @@
 #' set.seed(4)
 #' fold_changes = sample(c(0.5, 1, 2), size=numtx, prob=c(0.05, 0.9, 0.05), replace=TRUE)
 #' 
-#' simulate_experiment(fastapath, reads_per_transcript=10, fold_changes=fold_changes, outdir="./simdata/")
+#' simulate_experiment(fastapath, reads_per_transcript=10, fold_changes=fold_changes, 
+#'     outdir="./simdata/")
 #'
-#'}
 simulate_experiment = function(fasta, num_reps=10, fraglen=250, fragsd=25, 
     readlen=100, error_rate=0.005, paired=TRUE, reads_per_transcript=300, 
     fold_changes, dispersion_param=NULL, outdir=""){
