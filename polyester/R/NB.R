1ef97962a74c1207cc740147837ca5350b1e2758
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 13:37:43 -0400

    NB documentation

--- polyester/R/NB.R
+++ polyester/R/NB.R
@@ -1,13 +1,8 @@
-#'  Negative binomial draws
+#'  Draw nonzero negative binomial random numbers
 #'
-#'<full description>
-#' @param  basemeans <what param does>
-#' @param  dispersion_param <what param does>
-#' @param  seed=NULL <what param does>
-#' @export
-#' @examples \dontrun{
-#'
-#'}
+#' @param  basemeans vector of means, one per draw
+#' @param  dispersion_param vector of dispersion (\code{size}) parameters, one per draw
+#' @param  seed optional seed to set before drawing
 NB = function(basemeans, dispersion_param, seed=NULL){
     if(!is.null(seed)) set.seed(seed)
     numreads = rnbinom(n = length(basemeans), mu = basemeans, size = dispersion_param)
