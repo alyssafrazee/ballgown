a9b0c9bd20df74379092d0cac5d3a7e8271f10a8
Alyssa Frazee <acfrazee@gmail.com>
2014-04-15 16:55:16 -0400

    documents count_transcripts

--- polyester/R/count_transcripts.R
+++ polyester/R/count_transcripts.R
@@ -1,12 +1,13 @@
-#'<brief desc>
+# count the transcripts in a FASTA file
 #'
-#'<full description>
-#' @param fasta <what param does>
+#' determine how many transcripts are annotated in a FASTA file
+#' @param fasta character, path to a file in FASTA format
 #' @export
 #' @return Number of transcripts annotated in \code{fasta}
-#' @examples \dontrun{
+#' @examples 
+#' fastapath = system.file("data", "chr22.fa", package="polyester")
+#' count_transcripts(fastapath) #918
 #'
-#'}
 count_transcripts = function(fasta){
     length(readDNAStringSet(fasta))
 }
