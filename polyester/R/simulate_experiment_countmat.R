d913ef3fd1fe8469c9f61ccf1fcd023b4f95a909
Alyssa Frazee <acfrazee@gmail.com>
2014-04-16 10:46:52 -0400

    docs for simulate_experiment_countmat now respect the column<100 char rule

--- polyester/R/simulate_experiment_countmat.R
+++ polyester/R/simulate_experiment_countmat.R
@@ -4,12 +4,16 @@
 #' @param fasta path to FASTA file containing transcripts from which to simulate reads 
 #' @param readmat matrix with rows representing transcripts and columns representing samples. 
 #' Entry i,j specifies how many reads to simulate from transcript i for sample j.
-#' @param outdir character, path to folder where simulated reads should be written. Should end with "/" if specified. If unspecified, reads are written to the working directory.
-#' @param fraglen Mean RNA fragment length. Sequences will be read off the end(s) of these fragments.
+#' @param outdir character, path to folder where simulated reads should be written. Should end with 
+#' "/" if specified. If unspecified, reads are written to the working directory.
+#' @param fraglen Mean RNA fragment length. Sequences will be read off the end(s) of these 
+#' fragments.
 #' @param fragsd Standard deviation of fragment lengths. 
 #' @param readlen Read length
-#' @param error_rate=0.005 Sequencing error rate. Must be between 0 and 1. A uniform error model is assumed. 
-#' @param paired If \code{TRUE}, paired-end reads are simulated; else single-end reads are simulated.
+#' @param error_rate=0.005 Sequencing error rate. Must be between 0 and 1. A uniform error model is 
+#' assumed. 
+#' @param paired If \code{TRUE}, paired-end reads are simulated; else single-end reads are 
+#' simulated.
 #' @export
 #' @examples \dontrun{
 #' fastapath = system.file("data", "chr22.fa", package="polyester")
