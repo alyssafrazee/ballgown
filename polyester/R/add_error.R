27216d88a8336d14315014b889314ee0ddd2e681
Alyssa Frazee <acfrazee@gmail.com>
2014-04-17 15:09:16 -0400

    debugging messages (for now)

--- polyester/R/add_error.R
+++ polyester/R/add_error.R
@@ -10,11 +10,17 @@ add_error = function(tFrags, error_rate = 0.005){
     #^so you don't have to choose *another* nucleotide for an error: just choose *a* nucleotide.
     
     allSeq = unlist(tFrags)
+    print('allSeq:')
+    print(allSeq)
     insertLocs = Rle(sample(c(TRUE,FALSE), size = length(allSeq), 
            replace=TRUE, prob = c(adj_error, 1-adj_error)))
   
+    print('sum(insertLocs):')
+    print(sum(insertLocs))
     newletters = DNAString(
         paste(sample(c("A", "C", "G", "T"), sum(insertLocs), replace=TRUE), collapse="") )
+    print('newletters:')
+    print(newletters)
     allSeq = replaceLetterAt(allSeq, insertLocs, newletters)
     
     eFrags = DNAStringSet(allSeq, 
