8f36729f8b09542521492e3bd17e566199cfbfb4
Alyssa Frazee <acfrazee@gmail.com>
2014-05-05 21:27:28 -0400

    makes gffRead silent by default

--- R/gffRead.R
+++ R/gffRead.R
@@ -6,17 +6,21 @@
 #' @seealso \code{\link{getAttributeField}} to extract data from "attributes" column; \link{http://useast.ensembl.org/info/website/upload/gff.html} for more information on GTF/GFF files.
 #' @author Kasper Hansen
 #' @export
-gffRead <- function (gffFile, nrows = -1) 
+gffRead <- function (gffFile, nrows = -1, verbose=FALSE) 
 {
-    cat("Reading ", gffFile, ": ", sep = "")
+    if(verbose){
+        cat("Reading ", gffFile, ": ", sep = "")    
+    }
     gff = read.table(gffFile, sep = "\t", as.is = TRUE, quote = "", 
         header = FALSE, comment.char = "#", nrows = nrows, colClasses = c("character", 
             "character", "character", "integer", "integer", "character", 
             "character", "character", "character"))
     colnames(gff) = c("seqname", "source", "feature", "start", 
         "end", "score", "strand", "frame", "attributes")
-    cat("found", nrow(gff), "rows with classes:", paste(sapply(gff, 
-        class), collapse = ", "), "\n")
+    if(verbose){
+        cat("found", nrow(gff), "rows with classes:", paste(sapply(gff, 
+            class), collapse = ", "), "\n")
+    }
     stopifnot(!any(is.na(gff$start)), !any(is.na(gff$end)))
     return(gff)
 }
