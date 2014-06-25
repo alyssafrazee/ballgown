cfbd28e52f645e17632e9ac0206b9fb1f6d6e836
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-06-25 12:03:56 -0400

    replace tabindent with 4space

--- R/pctOverlap.R
+++ R/pctOverlap.R
@@ -17,11 +17,11 @@
 pctOverlap = function(tx1, tx2){
     ch1 = as.character(runValue(seqnames(tx1)))
     ch2 = as.character(runValue(seqnames(tx2)))
-	stopifnot(ch1 == ch2)
-	ntcov = coverage(c(tx1, tx2))
-	ind = which(names(ntcov)==ch1)
-	covrle = ntcov[[ind]]
-	covrle_val = runValue(covrle)
-	covrle_len = runLength(covrle)
-	return(sum(covrle_len[covrle_val==2]) / sum(covrle_len[covrle_val==1 | covrle_val == 2]))
+    stopifnot(ch1 == ch2)
+    ntcov = coverage(c(tx1, tx2))
+    ind = which(names(ntcov)==ch1)
+    covrle = ntcov[[ind]]
+    covrle_val = runValue(covrle)
+    covrle_len = runLength(covrle)
+    return(sum(covrle_len[covrle_val==2]) / sum(covrle_len[covrle_val==1 | covrle_val == 2]))
 }
