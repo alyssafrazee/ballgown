de9788879976609eb2ebecb7d40c38683a135a47
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-06-26 18:07:28 -0400

    refactors/speeds up/documents clusterTranscripts

--- R/clusterTranscripts.R
+++ R/clusterTranscripts.R
@@ -1,14 +1,14 @@
 #' group a gene's assembled transcripts into clusters
 #'
 #' @param gene name of gene whose transcripts will be clustered.  When using Cufflinks output, 
-#' usually of the form \code{"XLOC_######"}
+#'   usually of the form \code{"XLOC_######"}
 #' @param gown ballgown object containing experimental data
 #' @param k number of clusters to use
 #' @param method clustering method to use.  Must be one of \code{"hclust"}, for hierarchical 
-#' clustering, or \code{"kmeans"}, for k-means clustering. 
+#'   clustering, or \code{"kmeans"}, for k-means clustering. 
 #' 
 #' @return list with elements \code{clusters} and \code{pctvar}.  \code{clusters} contains columns 
-#' "cluster", "tname", and "tid", and denotes which transcripts belong to which clusters.  
+#' "cluster" and "t_id", and denotes which transcripts belong to which clusters.  
 #' \code{pctvar} is only non-NULL when using k-means clustering and is the percentage of variation 
 #' explained by these clusters, defined as the ratio of the between-cluster sum of squares to the 
 #' total sum of squares.
@@ -19,55 +19,50 @@
 #' @author Alyssa Frazee
 #' 
 #' @export
-clusterTranscripts = function(gene, gown, k=NULL, method=c("hclust", "kmeans")){
+#' 
+#' @examples 
+#' data(simgown)
+#' clusterTranscripts('XLOC_000454', simgown, k=2, method='kmeans')
+#' # transcripts 1294 and 1301 cluster together, 91% variation explained.
+
+clusterTranscripts = function(gene, gown, k=NULL, method=c('hclust', 'kmeans')){
     method = match.arg(method)
 
     txnames = indexes(gown)$t2g$t_id[indexes(gown)$t2g$g_id == gene]
-    strucnames = as.numeric(substr(names(structure(gown)$trans), 3, 
-        nchar(names(structure(gown)$trans))))
-    inds = which(strucnames %in% txnames)
+    if(is.null(k)){
+        k = ceiling(sqrt(length(txnames)/2))
+    }else{
+        if(length(txnames) <= k){
+            stop('k must be strictly less than the number of transcripts in gene')
+        }
+    }
+
+    inds = which(names(structure(gown)$trans) %in% txnames)
     tx = structure(gown)$trans[inds]
-    chr = texpr(gown, 'all')$chr[inds[1]] 
-    #TODO: add error check later, in case the tx's are on different chromosomes.
+    chr = texpr(gown, 'all')$chr[inds[1]]
 
-    covind = unique(as.numeric(lapply(tx, function(x) runValue(seqnames(x)))))
-    covs = lapply(tx, function(x) coverage(x)[[covind]])
-  
-    startpos = unlist(lapply(covs, function(x) runLength(x)[1]+1))
-    endpos = unlist(lapply(covs, function(x) sum(runLength(x))))
-    minbp = min(startpos)
-    maxbp = max(endpos)
+    xg = expand.grid(c(1:length(tx)), c(1:length(tx)))
+    matInds = as.vector(t(xg)) #vector of transcript pairs to calculate overlaps for
+    olGroup = rep(1:(length(matInds)/2), each=2)
+    olGroupSplit = rep(olGroup, times=elementLengths(tx[matInds]))
+    overlapping = split(unlist(tx[matInds]), olGroupSplit)
+    coverages = coverage(ranges(overlapping))
+    runvals = runValue(coverages)
+    runlengths = runLength(coverages)
+    pct = sum(runlengths[runvals==2]) / sum(runlengths[runvals==1 | runvals==2])
+    distMat = 1-matrix(pct, nrow=length(tx))
 
-    compactrles = lapply(covs, function(x){
-        Rle(values=runValue(x)[-1], lengths=runLength(x)[-1])
-    })
-    expanded = lapply(compactrles, IRanges::as.vector)
-    tnames = names(expanded)
-    expanded = lapply(1:length(expanded), function(i){
-        out = expanded[[i]]
-        if(startpos[i] > minbp){
-            out = c(rep(0, startpos[i]-minbp), out)
-        }
-        if(endpos[i] < maxbp){
-            out = c(out, rep(0, maxbp-endpos[i]))
-        }
-        return(out)
-    })
-
-    d = dist(matrix(unlist(expanded), nrow=length(expanded), byrow=TRUE))
-    if(is.null(k)) k = ceiling(sqrt(length(tnames)/2))
 
     if(method=="hclust"){
-        h = hclust(d)
-        groups = cutree(h, k=k)
+        groups = cutree(hclust(as.dist(distMat)), k=k)
         pctvar = NULL
     } 
     
     if(method=="kmeans"){
-        km = kmeans(as.matrix(d), centers=k)
+        km = kmeans(distMat, centers=k)
         groups = as.numeric(km$cluster)
         pctvar = km$betweenss/km$totss
     }
 
-    return(list(clusters=data.frame(cluster=groups, tname=tnames, tid=txnames), pctvar=pctvar))
+    return(list(clusters=data.frame(cluster=groups, t_id=txnames), pctvar=pctvar))
 }
