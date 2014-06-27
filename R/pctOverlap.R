8b034888caa8195f41064d6371f2b89e22af7ab1
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-06-27 16:14:28 -0400

    improves robustness of pctOverlap wrt overlapping ranges

--- R/pctOverlap.R
+++ R/pctOverlap.R
@@ -4,21 +4,32 @@
 #' @param tx2 GRanges object
 #' 
 #' @return percent overlap between \code{tx1} and \code{tx2}, as defined by the ratio of the 
-#' intersection of \code{tx1} and \code{tx2} to the union of \code{tx1} and \code{tx2}.
+#'   intersection of \code{tx1} and \code{tx2} to the union of \code{tx1} and \code{tx2}. 
 #' 
 #' @details In the ballgown context, \code{tx1} and \code{tx2} are two transcripts, each represented
-#' by GRanges objects whose ranges represent the exons comprising the transcripts.  The percent 
-#' overlap is the number of nucleotides falling within both transcripts divided by the number of 
-#' nucleotides falling within either transcript.  Useful as a measure of transcript closeness. 
+#'   by GRanges objects whose ranges represent the exons comprising the transcripts.  The percent 
+#'   overlap is the number of nucleotides falling within both transcripts divided by the number of 
+#'   nucleotides falling within either transcript.  Useful as a measure of transcript closeness. 
 
 #' @author Alyssa Frazee
 
 #' @export
 pctOverlap = function(tx1, tx2){
+    stopifnot(class(tx1) == 'GRanges' & class(tx2) == 'GRanges')
     ch1 = as.character(runValue(seqnames(tx1)))
     ch2 = as.character(runValue(seqnames(tx2)))
     if(ch1 != ch2){
         return(0)
+    }
+    tmp1 = reduce(tx1)
+    tmp2 = reduce(tx2)
+    if(!identical(ranges(tmp1), ranges(tx1))){
+        warning('tx1 contained overlapping ranges and was reduced.')
+        tx1 = tmp1
+    }
+    if(!identical(ranges(tmp2), ranges(tmp2))){
+        warning('tx2 contained overlapping ranges and was reduced.')
+        tx2 = tmp2
     }
     mcols(tx1) = NULL
     mcols(tx2) = NULL # take away metadata
