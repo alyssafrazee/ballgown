cd1cb334c59d815dfe549b976ef40e2962750145
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-07-10 15:48:19 -0400

    adds vignette, deletes old files

--- R/simgown-data.R
+++
@@ -1,7 +0,0 @@
-#' @name simgown
-#' @title Toy ballgown object
-#' @description Small ballgown object created with simulated toy data, for demonstration purposes
-#' @docType data
-#' @format a \code{ballgown} object: 100 transcripts, 633 exons, 536 introns
-#' @author Alyssa Frazee
-NULL
