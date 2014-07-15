468a89cbf3f908d0a47d50473bdb9882be6ed36c
Alyssa Frazee <alyssa.frazee@gmail.com>
2014-07-15 16:01:03 -0400

    deletes docs for ss (internal)

--- R/ss.R
+++ R/ss.R
@@ -1,14 +1,3 @@
-#' nice wrapper for string splitting and subsetting
-#'
-#' @param x character vector whose elements are to be split
-#' @param pattern what to split by
-#' @param slot which slot from each element of x you'd like returned (default 1)
-#' @param ... other arguments to strsplit
-#' @details internal use only 
-#' @author Andrew Jaffe
-#' @examples
-#' x = c('sample1.FPKM', 'sample1.cov', 'sample2.FPKM', 'sample2.cov')
-#' ss(x, pattern='\\.', slot=1) # sample IDs from x
 ss = function(x, pattern, slot=1, ...){
     sapply(strsplit(x,pattern,...), "[", slot)  
 } 
